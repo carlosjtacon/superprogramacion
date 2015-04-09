4c71d5795f990ab0b9743a067229ce6a2e50a8bd
Mario Rios <mrm994@gmail.com>
2015-04-09 22:17:05 +0200

    print working

--- PECL2/src/backend/Main.scala
+++ PECL2/src/backend/Main.scala
@@ -3,5 +3,7 @@ package backend
 object Main {
   def main(args: Array[String]) {
     println("Hello, world!")
+    val tab = new Tablero(4)
+    tab.print
   }
 }
--- PECL2/src/backend/Tablero.scala
+++ PECL2/src/backend/Tablero.scala
@@ -1,5 +1,25 @@
 package backend
+import scala.util.Random
 
-class Tablero {
-
+class Tablero(dificulty:Int) {
+  //val dificulty = _dificulty
+  val content = fila::fila::fila::fila::fila::fila::fila::fila::Nil
+  def random = Random.nextInt(dificulty)
+  
+  def fila:List[Int] = {
+    random::random::random::random::random::random::random::random::Nil
+  }
+  
+  //Funciones de imprimir
+  def print = print_aux(content)
+  def print_aux(l:List[List[Int]]):Unit = {
+    if (!l.isEmpty) {
+      println(print_sub_list(l.head))
+      print_aux(l.tail)  
+    }    
+  }
+  def print_sub_list(l:List[Int]):String = {
+    if(l.size == 1) l.head.toString()
+    else l.head.toString + " " +print_sub_list(l.tail)
+  }
 }
