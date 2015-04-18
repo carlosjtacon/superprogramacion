02f72646007208c9198075d117c96722c55cb9b1
Carlos Tacón <carlosjtacon@gmail.com>
2015-04-18 18:04:30 +0200

    cambio de iconos en los botones (sin controlar)

--- PECL2/src/frontend/App.java
+++ PECL2/src/frontend/App.java
@@ -3,15 +3,21 @@ package frontend;
 import java.awt.EventQueue;
 import java.util.List;
 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 import backend.*;
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class App {
 
 	private JFrame frmStarCrush;
+	private JButton buttons[];
+	private int change_aux;
 
 	/**
 	 * Launch the application.
@@ -50,30 +56,46 @@ public class App {
 		frmStarCrush.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmStarCrush.getContentPane().setLayout(null);
 		
-//		int i = 0;
-
-		JButton buttons[];
+		
+		change_aux = (-1);
 		int[] pos = new int[] {5, 50, 95, 140, 185, 230, 275, 320};
 		buttons = new JButton[tablist.size()];
 		for (int i=0; i<8; i++) {
 			for(int j=0; j<8; j++) {
 				buttons[i*8+j] = new JButton();
 				buttons[i*8+j].setIcon(new ImageIcon("resources/"+ tablist.get(i*8+j).toString() +".png"));
+				buttons[i*8+j].setToolTipText(String.valueOf(i*8+j)); 
 				buttons[i*8+j].setOpaque(false);
 				buttons[i*8+j].setContentAreaFilled(false);
 				buttons[i*8+j].setBorderPainted(false);
 				buttons[i*8+j].setFocusPainted(false);
-				buttons[i*8+j].setBounds(pos[i], pos[j], 40, 40);
+				
+				buttons[i*8+j].addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						JButton b = (JButton) e.getSource();
+						System.out.println(b.getToolTipText());
+						if(change_aux == (-1)) {
+							change_aux = Integer.valueOf(b.getToolTipText()); 
+						} else {
+							Icon ic = buttons[change_aux].getIcon();
+							buttons[change_aux].setIcon(b.getIcon());
+							buttons[Integer.valueOf(b.getToolTipText())].setIcon(ic);
+							change_aux = (-1);
+						}
+					}
+				});
+				
+				buttons[i*8+j].setBounds(pos[j], pos[i], 40, 40);
 				frmStarCrush.getContentPane().add(buttons[i*8+j]);
 			}
 		}
 		
 		JButton btnTiempo = new JButton("Tiempo");
-		btnTiempo.setBounds(5, 374, 175, 30);
+		btnTiempo.setBounds(5, 375, 175, 30);
 		frmStarCrush.getContentPane().add(btnTiempo);
 		
 		JButton btnMovimientos = new JButton("Movimientos");
-		btnMovimientos.setBounds(185, 374, 175, 30);
+		btnMovimientos.setBounds(185, 375, 175, 30);
 		frmStarCrush.getContentPane().add(btnMovimientos);
 	}
 
